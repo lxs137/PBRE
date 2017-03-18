948b48f32d08dddd2a008151c11ee89b9685294a
lxs <lxs137@hotmail.com>
2017-03-18 23:17:44 +0800

    add base geometry

--- src/util/geometry.h
+++ src/util/geometry.h
@@ -6,6 +6,12 @@
 class Vector3D{
 public:
     //method
+    Vector3D();
+    Vector3D(float xx, float yy, float zz):x(xx), y(xx), z(zz) {}
+    Vector3D(const Vector3D &v){
+        x = v.x, y = v.y, z = v.z;
+    }
+    virtual ~Vector3D();
 
 
     //value
@@ -19,8 +25,8 @@ public:
     Point3D(float xx, float yy, float zz):x(xx), y(yy), z(zz) {}
     Point3D(const Point3D &p){
         x = p.x, y = p.y, z = p.z;
-
     }
+    virtual ~Point3D();
 
     //value
     float x, y, z;
