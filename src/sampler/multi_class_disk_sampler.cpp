6740532f18e0bed6723a7fd8b1db51d8a3468a15
lxs <lxs137@hotmail.com>
2017-04-26 01:12:48 +0800

    BuildRMatrix

--- src/sampler/multi_class_disk_sampler.cpp
+++ src/sampler/multi_class_disk_sampler.cpp
@@ -28,10 +28,22 @@ void MultiClassDiskSampler::build_matrix_r()
         }
     }
     int P_size = P.size()/2;
-    std::vector<int> C;
-    float D;
+    std::vector<int> C, Pk;
+    C.reserve(class_n);
+    float D = 0.f;
     for(int k = 0; k < P_size; k++)
     {
-
+        for(int index = P[2 * k], n = P[2 * k + 1]; index <= n; index++)
+            Pk.push_back(index);
+        C.insert(C.end(), Pk.begin(), Pk.end());
+        for(int i : Pk)
+            D += 1.f/(distance[i] * distance[i]);
+        for(int i : Pk) {
+            for(int j : C) {
+                if(i != j)
+                    r[i][j] = r[j][i] = 1.f/std::sqrt(D);
+            }
+        }
+        Pk.clear();
     }
 }
